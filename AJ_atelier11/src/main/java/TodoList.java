65b2391726899a4bd8f846cf0c2b231ad79d5ef1
Asox7 <mommensylann@gmail.com>
2023-11-30 13:09:31 +0100

    FEAT(AJ10): 2.3 tutoriel implémentation tdd

---
+++ AJ_atelier11/src/main/java/TodoList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+    private List<String> tasks = new ArrayList<>();
+    public boolean addTask(String t){
+        if (t == null)
+            return false;
+        if(t.isBlank())
+            return false;
+        if(tasks.contains(t))
+            return false;
+        return tasks.add(t);
+    }
+
+    public boolean containsTask(String t) {
+        return tasks.contains(t);
+    }
+}
---
+++ AJ_atelier11/src/test/java/TodoListTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class TodoListTest {
+
+    private TodoList todoList;
+
+    @BeforeEach
+    void setUp() {
+        todoList = new TodoList();
+    }
+
+    @Test
+    void addTask() {
+        assertAll(
+                () -> assertTrue(todoList.addTask("task 1")),
+                () -> assertTrue(todoList.containsTask("task 1"))
+        );
+    }
+
+    @Test
+    void addEmptyTask() {
+        assertAll(
+                () -> assertFalse(todoList.addTask("")),
+                () -> assertFalse(todoList.containsTask("")),
+                () -> assertFalse(todoList.addTask(null)),
+                () -> assertFalse(todoList.containsTask(null))
+        );
+    }
+
+    @Test void addExistingTask() {
+        todoList.addTask("task 1");
+        assertFalse(todoList.addTask("task 1"));
+    }
+}
